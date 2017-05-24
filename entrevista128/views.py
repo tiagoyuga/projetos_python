4b592d2c0bd56c8051b9e869e9dc2606bcf1bf22
tiagoyuga <tiagoteixeira2214@gmail.com>
2017-05-24 18:51:54 -0300

    Atualização do projeto

--- entrevista128/views.py
+++ entrevista128/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Modelo128
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'entrevista128/post_list.html', {})
+def modelo_view(request):
+	dadosModelo = Modelo128.objects.all()#filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'entrevista128/modelo128_view.html', {'dadosModelo':dadosModelo})
+
+def post_create(request):
+	objModelo = Modelo128.objects.create(campo1='', campo2='')
+	return render(request, 'entrevista128/modelo128_view.html', {'objModelo' : objModelo})
+
+
+#Modelo128.objects.all()#todos
+#Modelo128.objects.get(campo='valor')#filtro
+#Modelo128.objects.filter(campo=valor)#filtro
+
+#salvar
+#obj = Modelo128.objects.get(id=1)
+#obj.publish()
+
+#ordenando
+#Modelo128.objects.order_by('campo')
+
+#ordenando invertido - basta acrescentar sinal de '-'
+#Modelo128.objects.order_by('-campo')
